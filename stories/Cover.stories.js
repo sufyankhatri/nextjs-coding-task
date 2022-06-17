68c13ca766951060458f26133439b6cd5961ef8a
sufyankhatri <sufyankhatree@gmail.com>
2022-06-17 12:20:40 +0500

    changes

---
+++ stories/Cover.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Cover from "../components/Cover";
+
+export default {
+  title: "Components/Cover",
+  component: Cover,
+  argTypes: {},
+};
+
+const Template = (args) => <Cover {...args} />;
+
