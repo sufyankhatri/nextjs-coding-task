157acf03ce466120dcc6ab56c58d2c72fb384cb8
sufyankhatri <sufyankhatree@gmail.com>
2022-06-18 19:15:36 +0500

    changes

--- src/stories/Cover.stories.js
+++ src/stories/Cover.stories.js
@@ -1,20 +1,3 @@
-// import React from 'react';
-// import Cover from '../components/Cover';
-
-// export default {
-//   title: 'Components/Cover',
-//   component: Cover,
-//   argTypes: {},
-// };
-
-// const Template = args => <Cover {...args} />;
-
-// export const CoverMain = Template.bind({});
-// CoverMain.args = {
-//   primary: true,
-//   children: "Contact me",
-// };
-
 import React from "react";
 import Cover from '../src/components/Cover';
 
